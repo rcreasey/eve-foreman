a460e0576f129ae87095e27573b3f609b0104e1e
Ryan C. Creasey <rcreasey@blizzard.com>
2013-05-29 14:26:07 -0700

    Adding in db credentials.

--- config/config.js
+++ config/config.js
@@ -3,17 +3,21 @@ module.exports = {
     root: require('path').normalize(__dirname + '/..'),
     db: 'mongodb://localhost/eveforeman',
     app: {
-      secret: 's3kr3t_34t1ng'
+      secret: 'dev'
     }
   },
   test: {
     root: require('path').normalize(__dirname + '/..'),
     db: 'mongodb://localhost/eveforeman_test',
     app: {
-      secret: 'testing'
+      secret: 'test'
     }
   },
   production: {
-
+    root: require('path').normalize(__dirname + '/..'),
+    db: 'mongodb://foreman:foremaneve@dbh35.mongolab.com:27357/heroku_app15945165',
+    app: {
+      secret: 's3kr3t_34t1ng'
+    }
   }
 }
